14c1fc93a3a459ca47029318a3302d99249e9884
abin <abin.z@outlook.com>
2025-04-23 23:42:09 +0800

    feat: 添加使用案例

---
+++ examples/pool_multiple_tasks.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "thread_pool/thread_pool.h"
+
+int main()
+{
+  abin::threadpool pool(30);  // 创建一个包含30个线程的线程池
+  std::cout << "===Thread pool created.===\n";
+  std::vector<std::future<int>> futures;
+  for (int i = 0; i < 100; ++i)
+  {
+    futures.emplace_back(pool.submit([i] {
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));  // 模拟任务执行时间
+      std::string msg = "Task " + std::to_string(i) + " is executed.\n";
+      std::cout << msg;
+      return i * i;
+    }));
+  }
+  std::cout << "===All tasks submitted.===\n";
+  for (auto &future : futures)
+  {
+    future.wait();  // 等待所有任务完成, 可以使用 std::future::get() 获取结果
+  }
+  std::cout << "===All tasks completed.===\n";
+}
---
+++ examples/pool_shutdown.cpp
@@ -0,0 +1,19 @@
+#include <thread_pool/thread_pool.h>
+
+#include <iostream>
+
+int main()
+{
+  abin::threadpool pool(4);     // 创建一个包含4个线程的线程池
+  for (int i = 0; i < 10; ++i)  // 提交10个任务
+  {
+    pool.submit([] {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      std::cout << "Should not always appear\n";
+    });
+  }
+  std::cout << "===All tasks submitted.===\n";
+  // pool.shutdown();  // 关闭线程池, 会等待所有任务完成
+  pool.shutdown(abin::threadpool::shutdown_mode::DiscardPendingTasks);  // 关闭线程池, 丢弃未开始的任务
+  std::cout << "===Thread pool shutdown.===\n";
+}
